0603b68d2db1e12e1acc6d2c2cf30314b7621e03
xiaoweixin8 <1205650919@qq.com>
2026-08-02 18:01:12 +0800

    部署脚本不需要运行e2e测试

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath } from "node:url"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  test: {
+    exclude: ["e2e/**", "node_modules/**", "dist/**", ".next/**"],
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./", import.meta.url)),
